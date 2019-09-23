ae1b981eb700803e6f43e7fe3c0de51cf931b5f0
Newton Mugo <newtonmugo@gmail.com>
2019-09-23 19:38:29 +0300

    Relationship between Product and Review Models

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Model;
+use App\Model\Review;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    function reviews () {
+        return $this->hasMany(Review::class);
+    }
 }
